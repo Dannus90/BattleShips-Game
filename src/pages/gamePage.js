991d9df8f62dc82223d83547bbf0a1609e69a711
Dannus90 <persson.daniel.1990@gmail.com>
2020-08-12 03:46:09 +0200

    First random ship generated

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./gamePage.styles.scss";
 import produce from "immer";
-import { v4 as uuidv4 } from "uuid";
 
 //GameBoard size
 const rows = 14;
@@ -20,26 +19,67 @@ const createGameBoard = () => {
 // Used for checking if a square has been clicked or not.
 const numbersArray = [];
 
+let gridArray = createGameBoard();
+
 const GamePage = () => {
-    const [grid, setGrid] = useState(createGameBoard());
+    // const [grid, setGrid] = useState(createGameBoard());
     const [score, setScore] = useState(0);
     const [numberOfTries, setNumberOfTries] = useState(80);
+
+    console.log(gridArray);
+
+    //Generate one random 5 point ship. Either Vertical or horizontal. Keeps it on the board
+    const generateFiveShip = (number) => {
+        
+        let randomDirection = Math.ceil(Math.random() * 2);
+        let randomStartGrid = Math.floor(Math.random() * gridArray.length);
+        let randomNumberGrid = Math.floor(Math.random() * gridArray.length);
+        if(randomDirection === 1 && randomNumberGrid + 1 <= gridArray.length && randomNumberGrid + 2 <= gridArray.length && randomNumberGrid + 3 <= gridArray.length && randomNumberGrid + 4 <= gridArray.length){
+        gridArray = produce(gridArray, (gridcopy) =>
+        {
+            
+            gridcopy[randomStartGrid][randomNumberGrid] = 1;
+            gridcopy[randomStartGrid][randomNumberGrid +1] = 1;
+            gridcopy[randomStartGrid][randomNumberGrid +2] = 1;
+            gridcopy[randomStartGrid][randomNumberGrid +3] = 1;
+            gridcopy[randomStartGrid][randomNumberGrid +4] = 1;
+        })
+        } else if(randomDirection === 2 && randomStartGrid + 1 <= gridArray.length && randomStartGrid + 2 <= gridArray.length && randomStartGrid + 3 <= gridArray.length && randomStartGrid + 4 <= gridArray.length) {
+            gridArray = produce(gridArray, (gridcopy) =>
+        {
+            
+            gridcopy[randomStartGrid][randomNumberGrid] = 1;
+            gridcopy[randomStartGrid +1][randomNumberGrid] = 1;
+            gridcopy[randomStartGrid +2][randomNumberGrid] = 1;
+            gridcopy[randomStartGrid +3][randomNumberGrid] = 1;
+            gridcopy[randomStartGrid +4][randomNumberGrid] = 1;
+        })
+        } else {
+            generateFiveShip();
+        }
+        
+
+        console.log(gridArray[randomStartGrid][randomNumberGrid]);
+        console.log(gridArray)
+    };
 
     //Random ships at start
     useEffect(() => {
         generateRandomShips();
+        generateFiveShip(2);
     }, []);
 
     //Function for generating random ships
     const generateRandomShips = () => {
-        const newGrid = produce(grid, (gridcopy) => {
-            for (let i = 0; i < grid.length; i++) {
+        gridArray = produce(gridArray, (gridcopy) => {
+            for (let i = 0; i < gridArray.length; i++) {
                 for (let j = 0; j < rows; j++) {
-                    gridcopy[i][j] = Math.round(Math.random() * 1);
+                    gridcopy[i][j] = 0;
+                    // gridcopy[i][j] = Math.round(Math.random() * 1);
                 }
             }
         });
-        setGrid(newGrid);
+        // setGrid(newGrid);
     };
 
     //Making sure we can only click a box once and the logic connected to it.
@@ -47,7 +87,7 @@ const GamePage = () => {
         if (numbersArray.includes(e.target.dataset.type)) {
             return;
         }
-        console.log(numbersArray);
+
         if (number) {
             e.target.classList.add("shipColumn");
             setScore((score) => score + 1);
@@ -68,7 +108,7 @@ const GamePage = () => {
                 }}
             >
                 {/* SETTING ALL INDIVIDUAL ITEMS! */}
-                {grid.map((row, rowIndex) => {
+                {gridArray.map((row, rowIndex) => {
                     return row.map((col, colIndex) => {
                         return (
                             <div
@@ -77,7 +117,7 @@ const GamePage = () => {
                                 }
                                 key={`${rowIndex}-${colIndex}`}
                                 onClick={(e) => {
-                                    fillBox(e, grid[rowIndex][colIndex]);
+                                    fillBox(e, gridArray[rowIndex][colIndex]);
                                 }}
                                 style={{
                                     width: 50,
